2023a6dc37b3492a861213c6b9849bc2fd79d35d
Akhil Seshan <akhilses@gmail.com>
2020-07-08 10:24:59 +0530

    removed confessions

--- confessions/assets/js/confessions.js
+++ confessions/assets/js/confessions.js
@@ -1,314 +1,49 @@
-confessions = [
-    {
-      "confession": "I will miss kuttan"
-    },
-    {
-      "confession": "College ended on March 10 and we didn't even realize it. We walked out of that classroom like it was just another day. If only I'd taken a few minutes to say goodbye."
-    },
-    {
-      "confession": "Boys ellam bismi pokana vibe miss cheyyum"
-    },
-    {
-      "confession": "I wish everyone in class replied in class group"
-    },
-    {
-      "confession": "Girls nu matram 1000 Rs treat kodukunna oru manyan anu Seshan"
-    },
-    {
-      "confession": "Seshaa, nee MEC vannattu aadhyam aayi electronics lab cheythappo orkando oru sir vannattu ennodu choichu\"oru photo idukatte, pottan of the year award tharaan aa\".Good times"
-    },
-    {
-      "confession": "Introduction day Shruti had a cold and it was super cute watching her half sniff and smile in the process."
-    },
-    {
-      "confession": "omae wa mou shindeiru"
-    },
-    {
-      "confession": "watashi wa mou shindeiru"
-    },
-    {
-      "confession": "I wish we could have one last lunch break, one last class photo, one last last period."
-    },
-    {
-      "confession": "Maybe should have taken the time to get to know everyone a lot better"
-    },
-    {
-      "confession": "Surprised by the effort certain people put for arts and sports this year kudos to them."
-    },
-    {
-      "confession": "Steve, you were my first crush."
-    },
-    {
-      "confession": "Stevinte graphics exam story aaanu best love story in csa"
-    },
-    {
-      "confession": "Tino's muscles are sexy"
-    },
-    {
-      "confession": "Tino!!! You are sooo boootifuuul!"
-    },
-    {
-      "confession": "Amruth bro thanks for taking care of me when I adichu bodham poyi on excel proshow night."
-    },
-    {
-      "confession": "Thadiya matte boxer bike aarnapol kore lift thannathin thankyouu"
-    },
-    {
-      "confession": "Kutta nee ineem orange curry maathram konduvarane"
-    },
-    {
-      "confession": "Don ninte koode ini turfil poya njan goalie nikoola annu ente ellam oru chavuttu kond chammanthi aakiyille"
-    },
-    {
-      "confession": "Sophus ore poli aane. I think someone loves you and you know it."
-    },
-    {
-      "confession": "Leon bhai examinu thala divasam vare nee paranju thann coa okke. Njan avasaanam passayi. Thanks bro"
-    },
-    {
-      "confession": "Pulachery is a chick magnet"
-    },
-    {
-      "confession": "Jimmychaa ninne thozhuthatt java supply ezhuthiyappo pass aayi."
-    },
-    {
-      "confession": "I don't know whether this will be valued, but I too had a crush on tino, my identity shall never be revealed"
-    },
-    {
-      "confession": "Tino achayan is looking for a soulmate with H1B visa"
-    },
-    {
-      "confession": "Imagine Pula and Pooja as a couple. Cute but debating skills"
-    },
-    {
-      "confession": "I really wish people would stop being two-faced. They treat you like a friend and say shit behind your back. To all of you, get a life. Gossiping may be fun and all for you but for the other person it might turn out to be a traumatic life experience."
-    },
-    {
-      "confession": "Kuriakose Motta adich kazhinju look koodi"
-    },
-    {
-      "confession": "Lomin sir's foss lab was the best, specially the cabin group"
-    },
-    {
-      "confession": "Shahul ES inte body entu rasama kanan. Njn classil irikkumbol shahuline thanne aanu nokane.. Code cheyyumbol muscles anangunath kandal ente sir ee chuttum ullathonnum kanan pattilla"
-    },
-    {
-      "confession": "Always wondered who Jisha will end up dating from class"
-    },
-    {
-      "confession": "Arts verrum myru aayirnu"
-    },
-    {
-      "confession": "Tbh, I wish our classmates were more active. It's shouldn't be like people like Abhishek tryin all the time to get everyone vibing. He's doing it all for CSA. It's just sad. Anyway, what's the point of saying it now. It was good while it lasted."
-    },
-    {
-      "confession": "Always wondered why diya and maman didn't end up as a couple. They had a vibe though."
-    },
-    {
-      "confession": "Akshay Prasannakumar is the coolest guy in CSA"
-    },
-    {
-      "confession": "Cutting class to watch oru pazhe bomb kadha was a mistake, Franco marakilla"
-    },
-    {
-      "confession": "Renjith, did u really love jumana miss?"
-    },
-    {
-      "confession": "Satya is pure at heart."
-    },
-    {
-      "confession": "Aravind is a soft person from inside. Don't be harsh on yourselves."
-    },
-    {
-      "confession": "Michelle has the best face and heart"
-    },
-    {
-      "confession": "MidhunBindiya Aparna ashwin NithinSruthy Anyone more left to confess?"
-    },
-    {
-      "confession": "Is it true that we have lesbian couples in class?"
-    },
-    {
-      "confession": "To meenu Maramkettam aanu ivalde main{}"
-    },
-    {
-      "confession": "Franco doesn't like fish"
-    },
-    {
-      "confession": "This is Oru Mandan dude signing off"
-    },
-    {
-      "confession": "No comments #1"
-    },
-    {
-      "confession": "No comments #2"
-    },
-    {
-      "confession": "I grew a lot as a person because of all of you. I never really had a connection with my peers at school. But after coming here, I found a home away from home"
-    },
-    {
-      "confession": "Ikka Make me one of your bivees️"
-    },
-    {
-      "confession": "Pada is a lucky guy"
-    },
-    {
-      "confession": "Ikka, code me like you code those french kagglers"
-    },
-    {
-      "confession": "Mayor was my first crush, but he's not my type"
-    },
-    {
-      "confession": "Does tino not like girls"
-    },
-    {
-      "confession": "College is overrated"
-    },
-    {
-      "confession": "Truth hurts and it should be said , but it can always be said in a polite manner. Words stay long after they are said."
-    },
-    {
-      "confession": "Tino likes to hangout with tall boys,idk why tho"
-    },
-    {
-      "confession": "No, they don't make a good couple!"
-    },
-    {
-      "confession": "Shahul and shebash are in love"
-    },
-    {
-      "confession": "I have a crush on Don from second year onwards and was hoping to confess during farewell."
-    },
-    {
-      "confession": "Iype doesn't sleep he teleports to another dimension."
-    },
-    {
-      "confession": "Class le main kozhi Vishnu aanu🥱"
-    },
-    {
-      "confession": "Jisha For 4 year I've seen how you liven up the room with your laugh, slowly falling for it. Touché it has all come to an end."
-    },
-    {
-      "confession": "Diyammaaaa Ninake oru aayiram.... alla Pathinaayiram....koranje poyale Oru kakkatholaayiram chakara ummas ️"
-    },
-    {
-      "confession": "Entha epo parayaa"
-    },
-    {
-      "confession": "Mec we love you"
-    },
-    {
-      "confession": "Cobra brothers"
-    },
-    {
-      "confession": "254 chars are not enough to describe Psy S A. During the past 4 years you have given me laughter, jealousy, love and hate. There has been both good and bad memories but today when i look back... It was a hell of a ride. Grateful that I was a part of CSA"
-    },
-    {
-      "confession": "I am your daddy sophus"
-    },
-    {
-      "confession": "To Thannimathan.. Y choo kute?"
-    },
-    {
-      "confession": "To Nikki........... Dhurantham parachil aan ivalde main{}"
-    },
-    {
-      "confession": "Hey gay3, Mec il mathram kand varunna oru prethyeka tharam kattu ind.. ath csb ne thazhuki csA il ethum. Ool aa mudi azhichittal undallo sireee, pna aduth egum nikkan pattula.. athrakkum naattamaanu!.. i luv u dearee"
-    },
-    {
-      "confession": "Hey Anu.. Modhi ne next year enkilum thaazhe irakkan pattuvo?"
-    },
-    {
-      "confession": "I don't like any of you except maybe 3 of you guys."
-    },
-    {
-      "confession": "To pamb vaasu: Shu.. shuu... shu... shu......... Bhuddhi illenkilum velivu illaima ollathond kozhappola.."
-    },
-    {
-      "confession": "Sesha ninte sticker oke bore aavunind, please stop"
-    },
-    {
-      "confession": "I hate that there are so many groups in our class, I feel left out most of the time."
-    },
-    {
-      "confession": "Joel has a girlfriend"
-    },
-    {
-      "confession": "Maman was once watching mallu analyst videos"
-    },
-    {
-      "confession": "Motta gang uyir, baaki Elam myr"
-    },
-    {
-      "confession": "@nihal I miss getting your song recommendations, you're a cool guy, we should've hung out more."
-    },
-    {
-      "confession": "Thomas is BPL"
-    },
-    {
-      "confession": "Pachaalam bhasi ude pinthalamurakkari al kalakaari pachaalam sreelu"
-    },
-    {
-      "confession": "*le vaasu: shuu. . Shu... shu... ee pen aarudeya? Ith enik tharaamo? Ith njan eduthotte? Ith njan angu edukkuva"
-    },
-    {
-      "confession": "I fell in love with Adheep's mwone mood sticker"
-    },
-    {
-      "confession": "Me: Sneha.. stapler undo? Sneha: stapler ond.( twist) but... pin illa.. Me(inside): pin illenkil pna nthina sir stapler?"
-    },
-    {
-      "confession": "How is Sarah late even for online class???"
-    },
-    {
-      "confession": "One story I'll always remember is ashik's bus story"
-    },
-    {
-      "confession": "I was going thru somthng really bad during 2nd year and I haven't shared about it yet to anyone from our class. But what matters is that I was able to overcome that because of some gems in our class who tuk me and made me feel one of their . Loveyouall3k"
-    },
-    {
-      "confession": "CsA was a turning point in my life. I got more new friends, experiences, and knowledge too. I really enjoyed these 3 years that I spend there. Excel , arts and onam was too good. Thanks frnds to support me like this. Never forget the moments. Love u all"
-    },
-    {
-      "confession": "Renjith oru vikaarajeeviyanu"
-    },
-    {
-      "confession": "Chala mary for life @midu"
-    },
-    {
-      "confession": "Michele, you were the only love of my life. I could see the whole world in your eyes. I couldn't tell it you personally. Now I think I am ready to confess. Love and love for you."
-    },
-    {
-      "confession": "Mec=seshan's stickers"
-    },
-    {
-      "confession": "Sophus thadiyan aayi"
-    },
-    {
-      "confession": "Aravind thala shave cheyanarnu, nalla look aayene"
-    },
-    {
-      "confession": "Ashik in a train. The start of a beautiful story of giving and taking. Someone should make this a movie."
-    },
-    {
-      "confession": "Me and tino are having a secret relationship."
-    },
-    {
-      "confession": "JJ machan and Maman. Two of the most mature yet fun to be around people in our class. Glad to have been your friend."
-    },
-    {
-      "confession": "Bonaventure(Good luck)"
-    },
-    {
-      "confession": "Njan avalodu chodichapo, aval paranju, \"going strong\"!! Kallan tino!! Aarum ariyilla enn vijarichu!"
-    },
-    {
-      "confession": "Aravind motta adich nalla look aayirikkum.... Enikkuvendi onnu adikuvo?"
-    },
-    {
-      "confession": "Stevinu colleginu purath relationship undennu kettath sathyamano?"
-    },
-    {
-      "confession": "Entha Kuriakose ithra jaada vallapozhum onnum chirich koode"
-    }
-  ]
+confessions = [{
+    "number":"1",
+    "confession": "njan oru mandan aanu"
+},
+{
+    "number":"1",
+    "confession": "njan oru mandan aanu"
+},
+{
+    "number":"1",
+    "confession": "njan oru mandan aanu"
+},
+{
+    "number":"1",
+    "confession": "njan oru mandan aanu"
+},
+{
+    "number":"1",
+    "confession": "njan oru mandan aanu"
+},
+{
+    "number":"1",
+    "confession": "njan oru mandan aanu"
+},
+{
+    "number":"1",
+    "confession": "njan oru mandan aanu"
+},
+{
+    "number":"1",
+    "confession": "njan oru mandan aanu"
+},
+{
+    "number":"1",
+    "confession": "njan oru mandan aanu"
+},
+{
+    "number":"1",
+    "confession": "njan oru mandan aanu"
+},
+{
+    "number":"1",
+    "confession": "njan oru mandan aanu"
+},
+{
+    "number":"1",
+    "confession": "njan oru mandan aanu"
+}
+]
