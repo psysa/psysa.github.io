2e718b000f4b48924e1bbf595d7438c593c8d789
Akhil Seshan <akhilses@gmail.com>
2020-07-11 13:34:35 +0530

    added date

---
+++ gallery/assets/js/gallery.js
@@ -0,0 +1,14 @@
+gallery = [{
+    '1':[
+        {
+            'image_link':'https://avatars2.githubusercontent.com/u/67822597?s=400&v=4',
+            'date': '24-June-2020',
+            'description':'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
+        },
+        {
+            'image_link':'https://avatars2.githubusercontent.com/u/67822597?s=400&v=4',
+            'date': '24-June-2020',
+            'description':'psysa2020 github page'
+        }
+    ]
+}];
